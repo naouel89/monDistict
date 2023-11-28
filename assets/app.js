73b9050f789d8874c6d2b5d2beae2bc95a4a879d
naouel89 <jessus.n@gmail.COM>
2023-11-28 11:26:15 +0100

    reactyarnsynfo

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+// assets/app.js
+
+// import logo from '../images/logo.png';
+// import '../css/styles.css';
+
+// const img = document.createElement('img');
+// img.src = logo;
+// document.body.appendChild(img);
